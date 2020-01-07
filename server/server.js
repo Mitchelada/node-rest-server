a9dc490f73f384bff39affc4e8f9abd27401cfb2
MichelAraya <michel.araya@live.com>
2020-01-06 23:43:01 -0300

    primer commit

---
+++ server/server.js
@@ -0,0 +1,54 @@
+require('./config/config')
+
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+
+//parse applcication/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+//parse applicaction/json
+app.use(bodyParser.json())
+
+
+app.get('/usuario', (req, res) => {
+    res.json("get usuario");
+})
+
+app.post('/usuario', (req, res) => {
+
+    let body = req.body
+
+    if (body.nombre === undefined) {
+
+        res.status(400).json({
+            ok: false,
+            mensaje: "El nombre es necesario"
+        })
+
+    } else {
+        res.json({
+            persona: body
+        });
+
+    }
+
+})
+
+app.put('/usuario/:id', (req, res) => {
+
+    let id = req.params.id; //request es la peticion de parametros
+
+    res.json({
+        id
+    })
+})
+
+app.delete('/usuario', (req, res) => {
+    res.json("delete usuario");
+})
+
+app.listen(process.env.PORT, () => {
+    console.log("Escuchando puerto: ", 3000);
+})
