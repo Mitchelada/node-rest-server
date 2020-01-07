bba595405e34d7090817b42fe47c5e00956f3c6c
MichelAraya <michel.araya@live.com>
2020-01-07 18:39:57 -0300

    agregando variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/cafe"
 } else {
-    urlDB = "mongodb+srv://deeper:kcpcwqGYk6aZY3fk@cluster0-pcg5p.mongodb.net/cafe"
+    urlDB = process.env.MONGO_URI
 }
 
 process.env.NODE_ENV = urlDB
